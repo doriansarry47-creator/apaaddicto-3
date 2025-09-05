487c9bc668169257b92cf9241366d0766816b63c
doriansarry47-creator <doriansarry47@gmail.com>
2025-09-05 11:32:44 +0200

    Ajout des composants principaux pour les pages Home, Login, Dashboard et NotFound

---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Apaaddicto 3</h1>
+      <p>This is the homepage of the application. Feel free to explore!</p>
+    </div>
+  );
+}
+
+export default HomePage;
